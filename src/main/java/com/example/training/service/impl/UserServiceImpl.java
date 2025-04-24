f1cf184a97357476d23e3cfa7a9338a80e634271
Ronald DC <rcdelacruz@gmail.com>
2025-04-24 14:34:53 +0800

    Fix import statement in UserServiceImpl

--- src/main/java/com/example/training/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/training/service/impl/UserServiceImpl.java
@@ -9,7 +9,7 @@ import com.example.training.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-timport org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
